38929dcf6d18189fb3fc85d415b670cd5f486320
Ankur Gupta <rukna1000@gmail.com>
2017-11-11 19:58:27 -0500

    Added imports, implemented methods, and added some good ol' overrides

--- src/IMDBMoviesGraph.java
+++ src/IMDBMoviesGraph.java
@@ -1,6 +1,21 @@
-public class IMDBMoviesGraph {
+import java.io.IOException;
+import java.util.Collection;
+
+public class IMDBMoviesGraph implements  Graph {
 
     public IMDBMoviesGraph (String actorsFilename, String actressesFilename) throws IOException {
         // Load data from the specified actorsFilename and actressesFilename ...
+    }
+
+    // TODO : This one
+    @Override
+    public Collection<? extends Node> getNodes() {
+        return null;
+    }
+
+    // TODO : THIS one
+    @Override
+    public Node getNodeByName(String name) {
+        return null;
     }
 }
