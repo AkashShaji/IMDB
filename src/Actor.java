6fc589e568322af152926429830701691b62dd04
Akash Shaji <akashshaji321@gmail.com>
2017-11-11 20:06:17 -0500

    created actor and movie classes

--- src/Actor.java
+++ src/Actor.java
@@ -1,2 +1,20 @@
-public class Actor {
+import java.util.ArrayList;
+
+public class Actor extends Node{
+    public String name;
+    public ArrayList<Movie> movies;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<Movie> getNeighbors(){
+        return this.movies;
+    }
+
+    public Actor(String name, ArrayList<Movie> movies){
+        this.name = name;
+        this.movies = movies;
+    }
+
 }
--- src/Movie.java
+++ src/Movie.java
@@ -1,2 +1,20 @@
-public class Movie {
+import java.util.ArrayList;
+
+public class Movie extends Node{
+    public String name;
+    public ArrayList<Actor> actors;
+
+    public String getName(){
+        return this.name;
+    }
+
+    public ArrayList<Actor> getNeighbors(){
+        return this.actors;
+    }
+
+    public Movie(String name, ArrayList<Actor> actors){
+        this.name = name;
+        this.actors = actors;
+    }
+
 }
