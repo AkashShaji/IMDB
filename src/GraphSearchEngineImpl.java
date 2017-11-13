dd2845353e118109d0b3e0cf43402cd5c69dd0d6
Akash Shaji <akashshaji321@gmail.com>
2017-11-12 21:18:36 -0500

    Added code so I could create the search engine

--- src/GraphSearchEngineImpl.java
+++ src/GraphSearchEngineImpl.java
@@ -1,9 +1,25 @@
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class GraphSearchEngineImpl implements GraphSearchEngine{
     public List<Node> findShortestPath (Node s, Node t){
+
         ArrayList<Node> sp = new ArrayList<Node>();
+        ArrayList<Node> queue = convertCollection(s.getNeighbors());
+
+        ArrayList<Node> searchedNodes = new ArrayList<Node>();
+
         return sp;
+    }
+
+    private ArrayList<Node> convertCollection(Collection<?extends Node> c){
+        ArrayList<Node> convertedCollection = new ArrayList<Node>();
+        for(Node n : c){
+            convertedCollection.add(n);
+        }
+        return convertedCollection;
     }
 }
