228e8f9cffabc8a37106c42f58cb237a17d8b5bf
Ankur Gupta <rukna1000@gmail.com>
2017-11-13 13:44:08 -0500

    Added some basic tests to the tester

--- tests/GraphCustomTester.java
+++ tests/GraphCustomTester.java
@@ -1,2 +1,30 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
 public class GraphCustomTester {
+
+    Graph _actorsGraph, _moviesGraph;
+    GraphSearchEngine _se;
+
+    @Before
+    public void init() throws IOException {
+        _actorsGraph = new IMDBActorsGraph("actors.list", "actresses.list");
+        _moviesGraph = new IMDBMoviesGraph("actors.list", "actresses.list");
+        _se = new GraphSearchEngineImpl();
+    }
+
+    @Test
+    public void finishedLoading() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void nodeVerification() {
+        System.out.println(_actorsGraph.getNodeByName("Jeremy, Ron"));
+    }
 }
