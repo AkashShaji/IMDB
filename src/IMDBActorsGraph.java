f83e4d2ef77afb6e0f837b971fcee1f387f72f97
Ankur Gupta <rukna1000@gmail.com>
2017-11-13 01:26:31 -0500

    Added getNodes() for the good actors Graph

--- src/IMDBActorsGraph.java
+++ src/IMDBActorsGraph.java
@@ -1,16 +1,25 @@
 import java.io.IOException;
 import java.util.Collection;
+import java.util.HashMap;
+import java.util.LinkedList;
 
 public class IMDBActorsGraph implements Graph {
 
+    private IMBDParser _parser;
+    private HashMap<String, Actor> _actors;
+
     public IMDBActorsGraph (String actorsFilename, String actressesFilename) throws IOException {
-        // Load data from the specified actorsFilename and actressesFilename ...
+        _parser = new IMBDParser(actorsFilename, actressesFilename);
+        _actors = _parser.getActors();
     }
 
-    // TODO: This one
     @Override
     public Collection<? extends Node> getNodes() {
-        return null;
+        final Collection<Node> actorsList = new LinkedList<>();
+
+        actorsList.addAll(_actors.values());
+
+        return actorsList;
     }
 
     // TODO: This one
